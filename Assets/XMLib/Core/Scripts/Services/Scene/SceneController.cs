2176187fd7cc068f9876326c909291c9c33bd0e1
XiangMu <xiangmu110@qq.com>
2018-07-06 18:04:47 +0800

    save

---
+++ Assets/XMLib/Core/Scripts/Services/Scene/SceneController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace XM.Services
+{
+    /// <summary>
+    /// 场景控制
+    /// </summary>
+    public class SceneController : MonoBehaviour
+    {
+    }
+}
---
+++ Assets/XMLib/Core/Scripts/Services/Scene/SceneService.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace XM.Services
+{
+    /// <summary>
+    /// 场景服务
+    /// </summary>
+    public class SceneService : IService
+    {
+        #region Base
+
+        protected override void OnAddService()
+        {
+        }
+
+        protected override void OnInitService()
+        {
+        }
+
+        protected override void OnRemoveService()
+        {
+        }
+
+        #endregion Base
+
+        #region Operation
+
+        public void Load(string sceneName)
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+
+        #endregion Operation
+    }
+}
