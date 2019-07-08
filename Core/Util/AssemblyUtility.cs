6a84bc02f751613327c6e418a76cf137e7e88759
XiangMu <xiangmu110@qq.com>
2019-07-08 22:18:28 +0800

    添加时间戳

--- Core/Util/AssemblyUtility.cs
+++ Core/Util/AssemblyUtility.cs
@@ -1,29 +1,39 @@
-﻿using System;
+﻿/*
+ * 作者：Peter Xiang
+ * 联系方式：565067150@qq.com
+ * 文档: https://github.com/xiangmu110/XMLib/wiki
+ * 创建时间: 2019/7/8 22:13:30
+ */
+
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
 using UnityEngine;
 
-public class AssemblyUtility
+namespace XMLib
 {
-    public static List<Type> FindAllSubclass<T>()
+    public class AssemblyUtility
     {
-        List<Type> results = new List<Type>();
-
-        Assembly[] assemblys = AppDomain.CurrentDomain.GetAssemblies();
-
-        foreach (var assembly in assemblys)
+        public static List<Type> FindAllSubclass<T>()
         {
-            Type[] types = assembly.GetTypes();
-            foreach (var type in types)
+            List<Type> results = new List<Type>();
+
+            Assembly[] assemblys = AppDomain.CurrentDomain.GetAssemblies();
+
+            foreach (var assembly in assemblys)
             {
-                if (!type.IsAbstract && type.IsSubclassOf(typeof(T)))
+                Type[] types = assembly.GetTypes();
+                foreach (var type in types)
                 {
-                    results.Add(type);
+                    if (!type.IsAbstract && type.IsSubclassOf(typeof(T)))
+                    {
+                        results.Add(type);
+                    }
                 }
             }
-        }
 
-        return results;
+            return results;
+        }
     }
 }
--- Core/Util/ZipUtil.cs
+++ Core/Util/ZipUtil.cs
@@ -1,72 +1,82 @@
-﻿using System;
+﻿/*
+ * 作者：Peter Xiang
+ * 联系方式：565067150@qq.com
+ * 文档: https://github.com/xiangmu110/XMLib/wiki
+ * 创建时间: 2019/7/8 22:13:30
+ */
+
+using System;
 using System.IO;
 using System.Text;
 
-/// <summary>
-/// 压缩工具
-/// </summary>
-public static class ZipUtil
+namespace XMLib
 {
-    public static byte[] Encode(string str)
+    /// <summary>
+    /// 压缩工具
+    /// </summary>
+    public static class ZipUtil
     {
-        byte[] buf = Encoding.UTF8.GetBytes(str);
-
-        byte[] outBuf = null;
-        using (MemoryStream outStream = new MemoryStream())
+        public static byte[] Encode(string str)
         {
-            using (MemoryStream inStream = new MemoryStream(buf))
+            byte[] buf = Encoding.UTF8.GetBytes(str);
+
+            byte[] outBuf = null;
+            using (MemoryStream outStream = new MemoryStream())
             {
-                SevenZip.Compression.LZMA.Encoder coder = new SevenZip.Compression.LZMA.Encoder();
+                using (MemoryStream inStream = new MemoryStream(buf))
+                {
+                    SevenZip.Compression.LZMA.Encoder coder = new SevenZip.Compression.LZMA.Encoder();
 
-                //写入长度
-                outStream.Write(BitConverter.GetBytes(inStream.Length), 0, 8);
+                    //写入长度
+                    outStream.Write(BitConverter.GetBytes(inStream.Length), 0, 8);
 
-                //写入属性
-                coder.WriteCoderProperties(outStream);
+                    //写入属性
+                    coder.WriteCoderProperties(outStream);
 
-                //压缩
-                coder.Code(inStream, outStream, inStream.Length, -1, null);
+                    //压缩
+                    coder.Code(inStream, outStream, inStream.Length, -1, null);
 
-                outStream.Flush();
+                    outStream.Flush();
 
-                outBuf = outStream.ToArray();
+                    outBuf = outStream.ToArray();
+                }
             }
+
+            return outBuf;
         }
 
-        return outBuf;
-    }
-
-    public static string Decode(byte[] buf)
-    {
-        string outStr = null;
-        using (MemoryStream outStream = new MemoryStream())
+        public static string Decode(byte[] buf)
         {
-            using (MemoryStream inStream = new MemoryStream(buf))
+            string outStr = null;
+            using (MemoryStream outStream = new MemoryStream())
             {
-                SevenZip.Compression.LZMA.Decoder coder = new SevenZip.Compression.LZMA.Decoder();
+                using (MemoryStream inStream = new MemoryStream(buf))
+                {
+                    SevenZip.Compression.LZMA.Decoder coder = new SevenZip.Compression.LZMA.Decoder();
 
-                //读取长度
-                byte[] fileLengthBytes = new byte[8];
-                inStream.Read(fileLengthBytes, 0, 8);
-                long fileLength = BitConverter.ToInt64(fileLengthBytes, 0);
+                    //读取长度
+                    byte[] fileLengthBytes = new byte[8];
+                    inStream.Read(fileLengthBytes, 0, 8);
+                    long fileLength = BitConverter.ToInt64(fileLengthBytes, 0);
 
-                //读取属性
-                byte[] properties = new byte[5];
-                inStream.Read(properties, 0, 5);
+                    //读取属性
+                    byte[] properties = new byte[5];
+                    inStream.Read(properties, 0, 5);
 
-                //设置属性
-                coder.SetDecoderProperties(properties);
+                    //设置属性
+                    coder.SetDecoderProperties(properties);
 
-                //解压
-                coder.Code(inStream, outStream, inStream.Length, fileLength, null);
+                    //解压
+                    coder.Code(inStream, outStream, inStream.Length, fileLength, null);
 
-                outStream.Flush();
+                    outStream.Flush();
 
-                byte[] outBuf = outStream.ToArray();
-                outStr = Encoding.UTF8.GetString(outBuf);
+                    byte[] outBuf = outStream.ToArray();
+                    outStr = Encoding.UTF8.GetString(outBuf);
+                }
             }
-        }
 
-        return outStr;
+            return outStr;
+        }
     }
 }
